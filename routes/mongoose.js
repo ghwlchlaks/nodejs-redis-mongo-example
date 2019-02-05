bbbea519ee28078c3dd8a3a2316ef11c24a46ad6
ghwlchlaks <ghwlchlaks@naver.com>
2019-02-06 02:39:31 +0900

    callback -> async await 진행 중
    /mongo/update async await 으로 변경

--- routes/mongoose.js
+++ routes/mongoose.js
@@ -30,21 +30,35 @@ router.get('/logout', (req, res) => {
   res.status(200).send('logout success');
 })
 
-router.put('/update', isAuthentication, (req, res) => {
-  const newData = req.body;
-  const originData = req.user;
-  Users.findOne({username: originData.username}, (findError, user) => {
-    if (findError) throw findError;
-    if (!user) res.send({status: true, msg: '해당 아이디가 존재 하지 않습니다.'})
+router.put('/update', isAuthentication, async (req, res) => {
+  try {
+    const newData = req.body;
+    const originData = req.user;
+    const user = await Users.findOne({username: originData.username})
+    if (!user) throw new Error('username not found');
     user.password = user.generateHash(newData.password);
     user.updateDate = Date.now();
-    user.save((err, saveUser) => {
-      if(err) throw err;
-      req.login(saveUser, (err) => {
-        if (err) throw err;
-      })
-      res.send({status: true, msg: '갱신 성공'});
-    })
-  })
+    const saveUser = await user.save()
+    req.login(saveUser, (err) => {
+      if (err) throw new Error(err);
+      throw new Error('req login failed');
+    });
+    return res.send({status: true, msg: '갱신 성공'});
+  } catch(err) {
+    return res.status(500).send({status: false, msg: 'error'});
+  }
+  // Users.findOne({username: originData.username}, (findError, user) => {
+  //   if (findError) throw findError;
+  //   if (!user) res.send({status: true, msg: '해당 아이디가 존재 하지 않습니다.'})
+  //   user.password = user.generateHash(newData.password);
+  //   user.updateDate = Date.now();
+  //   user.save((err, saveUser) => {
+  //     if(err) throw err;
+  //     req.login(saveUser, (err) => {
+  //       if (err) throw err;
+  //     })
+  //     res.send({status: true, msg: '갱신 성공'});
+  //   })
+  // })
 })
 module.exports = router;
